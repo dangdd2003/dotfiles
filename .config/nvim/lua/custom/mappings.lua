17caf2f49255d54cd3cd60bf278f89375061d750
Apollo-Dev <dangdoan2206@gmail.com>
2024-02-26 04:01:53 +0700

    add file browser for nvim telescope

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -3,9 +3,16 @@ local M = {}
 M.general = {
 }
 
+M.disabled = {
+  n = {
+    ["<leader>fb"] = "",
+  }
+}
+
 M.telescope = {
   n = {
     ["<leader>sk"] = { "<cmd> Telescope keymaps <CR>", "Fine keymaps" },
+    ["<leader>ff"] = { "<cmd> Telescope find_files follow=true no_ignore=true <CR>", "Find files" },
   }
 }
 
