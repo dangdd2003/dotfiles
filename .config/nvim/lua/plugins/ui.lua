6379e45c98d82345d228caa4ad23ad4bd604df39
Apollo-Dev <dangdoan2206@gmail.com>
2024-02-29 01:17:38 +0700

    colorizer for realtime color-code compiling

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -120,4 +120,20 @@ return {
       })
     end,
   },
+
+  -- colorizer - highlight corlor
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        RRGGBBAA = true,
+        AARRGGBB = true,
+        rgb_fn = true,
+        hsl_fn = true,
+        css = true,
+        css_fn = true,
+        tailwind = true,
+      },
+    },
+  },
 }
